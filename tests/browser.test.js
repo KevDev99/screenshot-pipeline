6698762fe942b6007e35f836b8327642e51973e7
TAUFER Kevin <kevin.taufer@hexagon.com>
2022-04-03 11:32:07 +0200

    changed test to close the browser in a afterAll - else jest throws a warning that the async operation didn't stopped after the testing finished

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -1,14 +1,19 @@
 const puppeteer = require("puppeteer");
 
-test("create screenshot", () => {
-  (async () => {
-    const browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    });
-    const page = await browser.newPage();
+describe("Browser testing", () => {
+  let browser;
+  let page;
+
+  beforeAll(async () => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+  });
+
+  it("make a screenshot", async () => {
     await page.goto("http://localhost:9999");
     await page.screenshot({ path: "screenshot.png" });
-
+  });
+  afterAll(async () => {
     await browser.close();
-  })();
+  });
 });
